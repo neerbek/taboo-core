87d8062593f73b9b25a7e4b6fabdb371266809cd
Jan Neerbek <jan.neerbek@alexandra.dk>
2017-04-07 10:47:48 +0200

    removed all qoute characters. Statparser is not handling in a nice way

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -25,13 +25,23 @@ class PreprocessingTest(unittest.TestCase):
     #modified copy of server_enron_helper.get_trees
 
     def test_nonascii(self):
-        inputfile="tests/resources/3.317398.KFV32XJXBEP5TEF42WHM3J1JO0BHARTVB.1.txt"
+        inputfile="tests/resources/texts/3.317398.KFV32XJXBEP5TEF42WHM3J1JO0BHARTVB.1.txt"
         enronLabel = EnronDocument.EnronLabel(problem="201", fileid=inputfile, strata=1000, relevance="0")
         enronText = EnronDocument.EnronText(enronLabel, inputfile)
         enronTexts = [enronText]
         enronTexts = server_enron_helper.load_text(enronTexts)
         text = enronTexts[0].text
         for c in ['#','\x0b','\x0e','\x0f','\x10','\x11','\x12','\x13','\x14','\x15','\x16','\x17','\x18','\x19','\x1A','\x1B','\x1C','\x1D','\x1E','\x1F']:
+            self.assertEqual(-1, text.find(c), "Expected doc to be cleaned. Found: {} at index {}".format(c, text.find(c)))
+
+    def test_removedquotes(self):
+        inputfile="tests/resources/texts/3.311758.AWACZMQ5U2ABLZMO0RFJOR5FHKAG5OI1B.txt"
+        enronLabel = EnronDocument.EnronLabel(problem="201", fileid=inputfile, strata=1000, relevance="0")
+        enronText = EnronDocument.EnronText(enronLabel, inputfile)
+        enronTexts = [enronText]
+        enronTexts = server_enron_helper.load_text(enronTexts)
+        text = enronTexts[0].text
+        for c in ['"']:
             self.assertEqual(-1, text.find(c), "Expected doc to be cleaned. Found: {} at index {}".format(c, text.find(c)))
 
 
