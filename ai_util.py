663dd9d616dbba6499923b14fda4399cb401ab09
Jan Neerbek <jan.neerbek@alexandra.dk>
2017-10-19 09:51:22 +0200

    beutify code

--- ai_util.py
+++ ai_util.py
@@ -6,54 +6,67 @@ Created on Sat Jan  7 08:16:27 2017
 """
 import time
 
+
 class Timer:
-    def __init__(self, name = "Timer"):
+    def __init__(self, name="Timer"):
         self.elapsed = 0
         self.start = None
         self.count = 0
         self.name = name
-    def begin(self):  #function cannot be named start
+
+    def begin(self):  # function cannot be named start
         self.start = time.time()
-    def end(self, count = 1):
-        if self.start!=None:
+
+    def end(self, count=1):
+        if self.start != None:
             end = time.time()
-            self.elapsed += end -self.start
+            self.elapsed += end - self.start
             self.count += count
             self.start = None
         else:
-            print("{} end() called, but timer is not started".format(self.name))
+            print(
+                "{} end() called, but timer is not started".format(self.name))
         return self
+
     def ratio(self):
         "seconds per count"
         if self.count == 0:
             return 0
         return (self.elapsed + 0.0) / self.count
+
     def report(self, count=None):
-        if count!=None:
+        if count != None:
             self.count = count
-        print("{}: Number of updates: {}. Total time: {:.2f}. Average time: {:.4f} sec".format(self.name, self.count, self.elapsed, self.ratio()))
+        print(
+            "{}: Number of updates: {}. Total time: {:.2f}. Average time: {:.4f} sec".
+            format(self.name, self.count, self.elapsed, self.ratio()))
+
 
 class TimerList:
     def __init__(self):
         self.timers = []
         self.lastReport = time.time()
+
     def addTimer(self, timer):
         self.timers.append(timer)
-    
+
     def end(self):
         for t in self.timers:
-            if t.start!=None:
+            if t.start != None:
                 t.end()
+
     def update(self):
         end = time.time()
         for t in self.timers:
-            if t.start!=None:
-                t.elapsed += end-t.start
+            if t.start != None:
+                t.elapsed += end - t.start
                 t.start = end
-    def do_report(self, min_seconds=-1, t = time.time()):
-        if (t-self.lastReport <min_seconds):
+
+    def do_report(self, min_seconds=-1, t=time.time()):
+        if (t - self.lastReport < min_seconds):
             return False
         return True
+
     def report(self, min_seconds=-1, update_timers=False):
         t = time.time()
         if not self.do_report(min_seconds, t):
