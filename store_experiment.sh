6086f8c4e011a2b511e86506f0d0784e29cb9876
Jan Neerbek <jan.neerbek@alexandra.dk>
2017-11-07 18:00:29 +0100

    added option for ignoring missing files

--- store_experiment.sh
+++ store_experiment.sh
@@ -1,41 +1,43 @@
 #!/bin/sh
 
 if [ "$1" = "" ]; then
-    echo "store_experiment.sh <name> [<y>]"
-    echo "if second argument is set, then log files and such are goind to be deleted after this script completes"
-  exit
+    echo "store_experiment.sh <name> [<y>] [ignore_missing]"
+    echo "if second argument is set to 'y', then log files and such are goind to be deleted after this script completes"
+    echo "if third argument is 'ignore_missing' then script continues even if some files are missing"
+    exit
 fi
 
-if [ "$2" != "" ]; then
-  echo "going to clean up directory"
+if [ "$2" = "y" ]; then
+    echo "going to clean up directory"
 fi
 
-if [ ! -f $1.log ]; then
-  echo "log file $1.log not found!"
-  exit 1
-fi
+if [ "$3" != "ignore_missing" ]; then
+    if [ ! -f $1.log ]; then
+        echo "log file $1.log not found!"
+        exit 1
+    fi
 
-if [ ! -f save_$1_running.txt ]; then
-  echo "no running model found!"
-  exit 1
-fi
+    if [ ! -f save_$1_running.txt ]; then
+        echo "no running model found!"
+        exit 1
+    fi
 
-if [ ! -f save_$1_best.txt ]; then
-  echo "no best model found!"
-  exit 1
-fi
+    if [ ! -f save_$1_best.txt ]; then
+        echo "no best model found!"
+        exit 1
+    fi
 
-if [ ! -d ../taboo-jan/functionality/logs/ ]; then
-  echo "log storage directory not found!"
-  exit 1
+    if [ ! -d ../taboo-jan/functionality/logs/ ]; then
+        echo "log storage directory not found!"
+        exit 1
+    fi
 fi
-
 
 zip $1.zip $1.log save_$1_*
 mv $1.zip ../taboo-jan/functionality/logs/
 svn add ../taboo-jan/functionality/logs/$1.zip
 svn ci -m "added log for $1" ..
-if [ "$2" != "" ]; then
+if [ "$2" = "y" ]; then
     rm $1rc
     rm $1.log
     rm save_$1_best.txt
