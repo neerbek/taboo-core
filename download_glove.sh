ba4b7a5e85be692f4bd73d412950f58e33403512
Jan Neerbek <jan.neerbek@cs.au.dk>
2019-05-30 13:32:27 +0200

    updated glove url

--- download_glove.sh
+++ download_glove.sh
@@ -3,7 +3,7 @@ cd ..
 mkdir code
 mkdir code/glove
 cd code/glove/
-curl -O https://nlp.stanford.edu/data/glove.6B.zip
+curl -O http://downloads.cs.stanford.edu/nlp/data/glove.6B.zip
 unzip glove.6B.zip
 cd ../..
 cd taboo-core
